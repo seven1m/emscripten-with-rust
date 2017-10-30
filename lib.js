612af745f12a324b25fc8dcdb4a55a95ae989f8e
Tim Morgan <tim@timmorgan.org>
2017-10-29 19:18:05 -0500

    Call js code as an API

---
+++ lib.js
@@ -0,0 +1,5 @@
+mergeInto(LibraryManager.library, {
+  hello_from_rust: function() {
+    console.log('hello from rust to JS!!!!');
+  }
+});
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use emscripten_sys::emscripten_run_script_string;
 
 use std::ffi::CString;
 
+extern "C" {
+  fn hello_from_rust();
+}
+
 fn main() {
     println!("Hello, Emscripten!");
-    let script = CString::new("alert('this is amazing')").unwrap();
+    let script = CString::new("console.log('eval string')").unwrap();
     let script_str_ptr = script.as_ptr();
     unsafe {
+        hello_from_rust();
         emscripten_run_script_string(script_str_ptr);
     }
 }
